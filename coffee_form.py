4907a10badd412ef19b77047d94239d4abc8a14b
pascal65536 <pascal65536@gmail.com>
2024-01-16 23:20:27 +0700

    modal coffee

--- coffee_form.py
+++ coffee_form.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QDialog
+import sys
+from PyQt5.QtWidgets import QDialog, QApplication
 from PyQt5 import uic
 
 
@@ -7,14 +8,30 @@ class CoffeeForm(QDialog):
         super().__init__()
         uic.loadUi("addEditCoffeeForm.ui", self)
         self.setModal(True)
+        self.cancelButton.clicked.connect(self.reject)
+        self.saveButton.clicked.connect(self.accept)
+        self.line_edit_variety.textChanged.connect(self.text_changed)
+        self.line_edit_roasting.textChanged.connect(self.text_changed)
+        self.line_edit_ground.textChanged.connect(self.text_changed)
+        self.line_edit_flavor.textChanged.connect(self.text_changed)
+        self.line_edit_price.textChanged.connect(self.text_changed)
+        self.line_edit_volume.textChanged.connect(self.text_changed)
+
+    def text_changed(self):
+        text_lst = [self.line_edit_variety.text(),
+                    self.line_edit_roasting.text(),
+                    self.line_edit_ground.text(),
+                    self.line_edit_flavor.text(),
+                    self.line_edit_price.text(),
+                    self.line_edit_volume.text()]
+        if text_lst.count(''):
+            self.saveButton.setEnabled(False)
+        else:
+            self.saveButton.setEnabled(True)
 
 
 if __name__ == '__main__':
-    import sys
-    from PyQt5.QtWidgets import QApplication
-    app = QApplication(sys.argv)
-    modal_form = CoffeeForm()
-    modal_form.exec_()
-    sys.exit(app.exec_())
-
-
+    app = QApplication([])
+    dialog = CoffeeForm()
+    result = dialog.exec_()
+    app.quit()
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import sqlite3
-from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidgetItem, QPushButton
+from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidgetItem, QDialog
 from PyQt5 import uic
 from coffee_form import CoffeeForm
 
@@ -19,6 +19,20 @@ class Coffe(QMainWindow):
     def initUI(self):
         self.setGeometry(800, 600, *SCREEN_SIZE)
         self.display_data()
+
+    def create_data(self, variety, roasting, ground, flavor, price, volume):
+        cur = self.con.cursor()
+        query = f"INSERT into COFFEE(variety, roasting, ground, flavor, price, volume) "
+        query += f"VALUES ({variety}, {roasting}, {ground}, {flavor}, {price}, {volume})"
+        cur.execute(query)
+        self.con.commit()
+
+    def update_data(self, coffee_id, variety, roasting, ground, flavor, price, volume):
+        cur = self.con.cursor()
+        query = f"UPDATE coffee SET variety = '{variety}', roasting = '{roasting}', ground = '{ground}', "
+        query += f"flavor = '{flavor}', price = '{price}', volume = '{volume}' WHERE id == {coffee_id}"
+        cur.execute(query)
+        self.con.commit()
 
     def display_data(self):
         cur = self.con.cursor()
@@ -39,27 +53,41 @@ class Coffe(QMainWindow):
 
     def openAddCoffeeForm(self):
         self.addCoffeeForm = CoffeeForm()
-        # self.addCoffeeForm.saveButton.clicked.connect(self.saveCoffee)
-        # self.addCoffeeForm.cancelButton.clicked.connect(self.saveCoffee)
         self.addCoffeeForm.exec_()
+        if self.addCoffeeForm.result() == QDialog.Accepted:
+            create_lst = []
+            create_lst.append(self.addCoffeeForm.line_edit_variety.text())
+            create_lst.append(self.addCoffeeForm.line_edit_roasting.text())
+            create_lst.append(self.addCoffeeForm.line_edit_ground.text())
+            create_lst.append(self.addCoffeeForm.line_edit_flavor.text())
+            create_lst.append(self.addCoffeeForm.line_edit_price.text())
+            create_lst.append(self.addCoffeeForm.line_edit_volume.text())
+            self.create_data(*create_lst)
+            self.display_data()
 
     def openEditCoffeeForm(self):
-        selectedRow = self.tableWidget.currentRow()
-        if selectedRow >= 0:
-            coffeeId = self.tableWidget.item(selectedRow, 0).text()
-            # Получите данные о кофе из базы данных по coffeeId
-            coffeeData = self.getCoffeeData(coffeeId)
-            if coffeeData:
-                self.editCoffeeForm = CoffeeForm()
-                self.editCoffeeForm.saveButton.clicked.connect(self.saveCoffee)
-                # Заполните поля формы данными о кофе
-                self.editCoffeeForm.nameLineEdit.setText(coffeeData['name'])
-                self.editCoffeeForm.descriptionTextEdit.setPlainText(coffeeData['description'])
-                self.editCoffeeForm.priceSpinBox.setValue(coffeeData['price'])
-                self.editCoffeeForm.exec_()
-        else:
-            # Предупреждение: нет выбранной строки в таблице
-            pass
+        selected_row = self.tableWidget.currentRow()
+        if selected_row < 0:
+            return
+        coffee_id = self.tableWidget.item(selected_row, 0).text()
+        self.editCoffeeForm = CoffeeForm()
+        self.editCoffeeForm.line_edit_variety.setText(self.tableWidget.item(selected_row, 1).text())
+        self.editCoffeeForm.line_edit_roasting.setText(self.tableWidget.item(selected_row, 2).text())
+        self.editCoffeeForm.line_edit_ground.setText(self.tableWidget.item(selected_row, 3).text())
+        self.editCoffeeForm.line_edit_flavor.setText(self.tableWidget.item(selected_row, 4).text())
+        self.editCoffeeForm.line_edit_price.setText(self.tableWidget.item(selected_row, 5).text())
+        self.editCoffeeForm.line_edit_volume.setText(self.tableWidget.item(selected_row, 6).text())
+        self.editCoffeeForm.exec_()
+        if self.editCoffeeForm.result() == QDialog.Accepted:
+            update_lst = [coffee_id]
+            update_lst.append(self.editCoffeeForm.line_edit_variety.text())
+            update_lst.append(self.editCoffeeForm.line_edit_roasting.text())
+            update_lst.append(self.editCoffeeForm.line_edit_ground.text())
+            update_lst.append(self.editCoffeeForm.line_edit_flavor.text())
+            update_lst.append(self.editCoffeeForm.line_edit_price.text())
+            update_lst.append(self.editCoffeeForm.line_edit_volume.text())
+            self.update_data(*update_lst)
+            self.display_data()
 
 
 if __name__ == "__main__":
